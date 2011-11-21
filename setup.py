4bc288bb1d49e1837da892f8f0a8b0550681ad76
Douglas Meehan <dmeehan@gmail.com>
2011-11-20 22:27:39 -0500

    added get_version to init and edited setup

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except (IOError, OSError):
 
 setup(
     name='django-backbeat-inlines',
-    version=images.get_version(),
+    version=inlines.get_version(),
     description='A pluggable inlines app.',
     long_description=long_description,
     author='Douglas Meehan',
